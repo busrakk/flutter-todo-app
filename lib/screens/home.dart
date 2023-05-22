3a4834ea5efd0ecdfd3c31f0aeb85a0a5172c5ce
busrakk <busrakartal160@gmail.com>
2023-05-22 17:17:52 +0300

    add to list

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -97,6 +97,54 @@ class _HomeScreenState extends State<HomeScreen> {
                 ]),
               )
             ]),
+          ),
+          Align(
+            // stack içinde konumlandırmak için bu widget kullanıldı
+            alignment: Alignment.bottomCenter,
+            child: Row(
+              children: [
+                Expanded(
+                    child: Container(
+                  margin:
+                      const EdgeInsets.only(bottom: 20, right: 20, left: 20),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: const [
+                        BoxShadow(
+                            color: Colors.grey,
+                            offset: Offset(0, 0),
+                            blurRadius: 10,
+                            spreadRadius: 0)
+                      ], // gölgelendirme
+                      borderRadius: BorderRadius.circular(10)),
+                  child: TextField(
+                    controller: _todoController,
+                    decoration: const InputDecoration(
+                        hintText: "Add a new todo item",
+                        border: InputBorder.none),
+                  ),
+                )),
+                // ekleme butonu
+                Container(
+                  margin: const EdgeInsets.only(bottom: 20, right: 20),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      _addTodoItem(_todoController.text);
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: tdBlue,
+                        minimumSize: const Size(60, 60),
+                        elevation: 10),
+                    child: const Text(
+                      '+',
+                      style: TextStyle(fontSize: 40),
+                    ),
+                  ),
+                )
+              ],
+            ),
           )
         ],
       ),
